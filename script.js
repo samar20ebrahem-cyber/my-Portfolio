c1a0eaa1db9b2b420248abbb144eefb555c5b832
sebrahem605-lang <sebrahem605@gmail.com>
2026-06-15 22:55:01 +0300

    add .js

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-// const navToggle = document.getElementById('nav_toggle');
-// const navClose = document.getElementById('nav_close');
-// const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav_toggle');
+const navClose = document.getElementById('nav_close');
+const navMenu = document.getElementById('nav-menu');
 
-// // فتح المنيو
-// if (navToggle && navMenu) {
-//     navToggle.addEventListener('click', () => {
-//         navMenu.classList.add('show-menu');
-//     });
-// }
+if (navToggle && navMenu) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
 
-// // إغلاق المنيو بالـ X
-// if (navClose && navMenu) {
-//     navClose.addEventListener('click', () => {
-//         navMenu.classList.remove('show-menu');
-//     });
-// }
+if (navClose && navMenu) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
-// // إغلاق المنيو عند الضغط على أي لينك
-// const navLinks = document.querySelectorAll('.nav_list li a');
-// navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//         if (navMenu) {
-//             navMenu.classList.remove('show-menu');
-//         }
-//     });
-// });
+const navLinks = document.querySelectorAll('.nav_list li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (navMenu) {
+            navMenu.classList.remove('show-menu');
+        }
+    });
+});
 
