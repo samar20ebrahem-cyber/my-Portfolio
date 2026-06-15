7b777153c16ec76907435772ab8a35f02a11f6d8
sebrahem605-lang <sebrahem605@gmail.com>
2026-06-15 22:41:21 +0300

    add .js , RESPONSIVE DESIGN (MOBILE)

---
+++ script.js
@@ -0,0 +1,28 @@
+// const navToggle = document.getElementById('nav_toggle');
+// const navClose = document.getElementById('nav_close');
+// const navMenu = document.getElementById('nav-menu');
+
+// // فتح المنيو
+// if (navToggle && navMenu) {
+//     navToggle.addEventListener('click', () => {
+//         navMenu.classList.add('show-menu');
+//     });
+// }
+
+// // إغلاق المنيو بالـ X
+// if (navClose && navMenu) {
+//     navClose.addEventListener('click', () => {
+//         navMenu.classList.remove('show-menu');
+//     });
+// }
+
+// // إغلاق المنيو عند الضغط على أي لينك
+// const navLinks = document.querySelectorAll('.nav_list li a');
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         if (navMenu) {
+//             navMenu.classList.remove('show-menu');
+//         }
+//     });
+// });
+
