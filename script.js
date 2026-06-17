7058379780fde2746ab15c6aaa2c38254533b2bf
sebrahem605-lang <sebrahem605@gmail.com>
2026-06-17 03:05:12 +0300

    change in input range

--- script.js
+++ script.js
@@ -26,7 +26,23 @@ navLinks.forEach(link => {
 
 // زرار ال اضافه البراجراف سيكشن عني 
 
+const addMoreP = document.getElementById('readMore');
+const readSecoundP = document.getElementById('read');
 
+if (addMoreP && readSecoundP) {
+    addMoreP.addEventListener('click', () => {
+        // بنشيك على الـ display الحالي للبراجراف
+        if (readSecoundP.style.display === 'block') {
+            // لو هو ظاهر فعلاً.. إذن اليوزر عاوز يقفل (Read Less)
+            readSecoundP.style.display = 'none';
+            addMoreP.textContent = 'Read More';
+        } else {
+            // لو هو مخفي (أو لسه الصفحة فاتحة).. إذن اليوزر عاوز يفتح (Read More)
+            readSecoundP.style.display = 'block';
+            addMoreP.textContent = 'Read Less';
+        }
+    });
+}
 
 
 //   هنضيف الاسبان حسب الرانج 
@@ -39,12 +55,19 @@ document.addEventListener("DOMContentLoaded", () => {
         const spanText = skill.querySelector("span");
 
         if (rangeInput && spanText) {
-            spanText.textContent = rangeInput.value + "%";
+            const updateProgress = () => {
+               const currentPercent = rangeInput.value;
+                spanText.textContent = currentPercent + "%";
+                rangeInput.style.background = `linear-gradient(to right, #c0268f 0%, #9F2B7E ${currentPercent}%, #2a1f4d ${currentPercent}%, #2a1f4d 100%)`;
+    };
+
+            updateProgress();
 
     //   لو شلتي الديسبول من الرانج الاسبان هيتحدث تلقائي
-            rangeInput.addEventListener("input", () => {
-                spanText.textContent = rangeInput.value + "%";
-            });
-        }
+            rangeInput.addEventListener("input", updateProgress);
+         }
     });
 });
+
+
+
