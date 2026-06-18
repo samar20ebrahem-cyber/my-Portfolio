293e181f21c350fa6a71d3311c91caf4deb24a0b
sebrahem605-lang <sebrahem605@gmail.com>
2026-06-18 19:17:23 +0300

    the end

--- script.js
+++ script.js
@@ -68,4 +68,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+// scrollToTopBtn
+const scrollTopBtn = document.getElementById("scrollToTopBtn");
 
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+}
+
+scrollTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
